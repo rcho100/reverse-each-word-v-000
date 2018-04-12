fa025df845822280b848c609c3bb2eb2a7361dc5
Richard Cho <richardcho100@gmail.com>
2018-04-12 03:14:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,12 @@ def reverse_each_word(sentence)
   array = sentence.split
   #binding.pry
   #array is ["Hello", "there,", "and", "how", "are", "you?"]
-  reverse_arr = []
-  array.each do |word|
-    reverse_arr << word.reverse
+  # reverse_arr = []
+  # array.each do |word|
+  #   reverse_arr << word.reverse
+  # end
+  # reverse_arr.join(" ")
+  array.collect do |word|
+    word.reverse
   end
-  reverse_arr.join(" ")
 end
